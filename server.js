f03448d6c141c074cd474d31bc324046cb2c5d9f
Biprodeep Bose <bipbose123@gmail.com>
2026-01-19 14:12:10 +0530

    ups

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const http = require('http');
 
 const PORT = 3000;
+
+const envTest = process.env.TEST_VARIABLE || 'Default Value';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   
   if (req.url === '/') {
-    res.end('<h1>Welcome to the Simple Server!</h1>');
+    res.end(`<h1>Welcome to the Simple Server! ${envTest}</h1>`);
   } else if (req.url === '/about') {
     res.end('<h1>About Page</h1><p>This is a simple Node.js server.</p>');
   } else {
